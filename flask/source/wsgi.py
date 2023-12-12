cd498966206723044f9f0e5057126db5fdfcf5a1
Alexandria <ejinu2011@gmail.com>
2023-12-12 15:14:26 +0000

    2023-12-13

--- flask/source/wsgi.py
+++ flask/source/wsgi.py
@@ -3,5 +3,4 @@ from my_app import create_app, sock
 app = create_app()
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0")
-    sock.run(app=app, host="0.0.0.0", allow_unsafe_werkzeug=True)
+    sock.run(app=app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
