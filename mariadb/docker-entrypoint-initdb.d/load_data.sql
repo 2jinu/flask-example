0e72528da3c5c79f2572f17b97355f3dfd64718f
Alexandria <ejinu2011@gmail.com>
2023-12-17 12:14:53 +0000

    2023-12-17

--- mariadb/docker-entrypoint-initdb.d/load_data.sql
+++ mariadb/docker-entrypoint-initdb.d/load_data.sql
@@ -1,5 +1,3 @@
 INSERT INTO roles (name) VALUES ('ADMIN'), ('USER');
 INSERT INTO users (username, password) VALUES ('admin', 'scrypt:32768:8:1$cps213Dl9oKmyhZD$0769090c38309b39109aba6a6ef077a747b245bcf2411b5aae18fc5a986ac2ab04276ec38a8ac6f1eb478e2b92305496c529f70176aff220575243f43cead8a4');
 INSERT INTO rel_user_roles (user_id, role_id) VALUES (1, 1);
-INSERT INTO users (username, password) VALUES ('user1', 'scrypt:32768:8:1$cps213Dl9oKmyhZD$0769090c38309b39109aba6a6ef077a747b245bcf2411b5aae18fc5a986ac2ab04276ec38a8ac6f1eb478e2b92305496c529f70176aff220575243f43cead8a4');
-INSERT INTO rel_user_roles (user_id, role_id) VALUES (2, 2);
